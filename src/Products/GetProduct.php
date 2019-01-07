be96885d48e67e64c83df217fc7fb6fb36bf40d4
Samuel Stidham <dqfan2012@gmail.com>
2019-01-07 11:41:54 -0500

    Added the Get Product API request and related unit tests.

---
+++ src/Products/GetProduct.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace dqfan2012\Sellbrite\Products;
+
+use Carbon\Carbon;
+use dqfan2012\Sellbrite\Core\Core;
+
+/**
+ * The Product API isn't documented enough. Will not use until it is.
+ */
+class GetProduct extends Core
+{
+    /**
+     * @param integer $page Page number
+     * @param integer $limit Number of results per page
+     * @param array   $skuArr An array of SKUs
+     */
+    public function sendRequest(
+        int $page = null,
+        int $limit = null,
+        array $skuArr = []
+    ) {
+        // Build the API endpoint
+        $url = self::BASE_URI . 'products';
+
+        // Build the API headers
+        $apiHeaders = $this->baseApiHeaders;
+        $apiHeaders['headers']['Content-Type'] = 'application/json';
+
+        // Add the page query string if necessary
+        if (is_null($page) === false && intval($page) > 0)
+        {
+            $apiHeaders['query']['page'] = $page;
+        }
+
+        // Add the limit query string is necessary. NOTE: The API limits returned order count
+        // to 100. The API will -not- allow more to be fetched by a single request.
+        if (is_null($limit) === false)
+        {
+            if (intval($limit) > 100) {
+                $apiHeaders['query']['limit'] = 100;
+            } else {
+                $apiHeaders['query']['limit'] = $limit;
+            }
+        }
+
+        if (is_array($skuArr) && empty($skuArr) === false) {
+            $apiHeaders['query']['skus'] = $skuArr;
+        }
+
+        // Send the HTTP request to the API endpoint and get the response stream
+        $response = $this->httpClient->request('GET', $url, $apiHeaders);
+
+        // Get the status code returned with the response
+        $statusCode = $response->getStatusCode();
+
+        // Check status code for success or failure
+        switch ($statusCode)
+        {
+            case 200:
+                // Returning the PSR7 response object because of the Total-Pages header. Will handle the
+                // total pages logic outside of this class
+                return $response;
+                break;
+            case 401:
+                throw new \Exception("401 Unauthorized - HTTP Basic: Access denied.");
+                break;
+            default:
+                throw new \Exception('This is the default error.');
+        }
+    } // End public function sendRequest
+} // End class GetProduct
---
+++ tests/Products/GetProductTest.php
@@ -0,0 +1,303 @@
+<?php
+
+declare(strict_types=1);
+
+namespace dqfan2012\Sellbrite\Test\Products;
+
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use dqfan2012\Sellbrite\Products\GetProduct;
+
+class GetProductTest extends TestCase
+{
+    public function testGetProductSuccessfullyGetProducts()
+    {
+        // Get the stored credentials
+        $accountToken = 'am2902ngt3Nn';
+        $secretKey    = 'happy28bananas';
+
+        // Create a mock client object
+        $mockClient = \Mockery::mock(ClientInterface::class);
+
+        // The mock client should receive a request call and it should return at PSR-7 Response object
+        // cotaining JSON
+        $mockClient->shouldReceive('request')->andReturns(
+            new Response(
+                200,
+                [ 'Content-Type' => 'application/json' ],
+                '[{
+                    "id": 1,
+                    "sku": "1903275",
+                    "asin": null,
+                    "condition": "used",
+                    "brand": null,
+                    "manufacturer": null,
+                    "manufacturer_model_number": null,
+                    "name": "Test Product",
+                    "description": null,
+                    "price": 0,
+                    "cost": 0,
+                    "package_length": 0,
+                    "package_width": 0,
+                    "package_height": 0,
+                    "package_unit_of_length": "inches",
+                    "package_weight": 0,
+                    "package_unit_of_weight": "pounds",
+                    "msrp": 0,
+                    "category_name": "",
+                    "features": [
+                        "yellow",
+                        "beautiful"
+                    ],
+                    "warranty": null,
+                    "condition_note": null,
+                    "upc": null,
+                    "ean": null,
+                    "isbn": null,
+                    "gtin": null,
+                    "gcid": null,
+                    "epid": null,
+                    "image_list": "",
+                    "custom_attributes": {
+                        "plate": "paper"
+                    }
+                }]'
+            )
+        );
+
+        $page = 1;
+        $limit = 100;
+        $skuArr = ['1903275'];
+
+        // Create a new instance of DeleteProduct
+        $getProduct = new GetProduct($accountToken, $secretKey, $mockClient);
+
+        // Send the request and get the response object
+        $response = $getProduct->sendRequest($page, $limit, $skuArr);
+
+        $responseJson = (string) $response->getBody();
+
+        // Assert the returned JSON response matches the expected data
+        $this->assertJsonStringEqualsJsonString(
+            $responseJson,
+            json_encode([
+                [
+                    'id' => 1,
+                    'sku' => '1903275',
+                    'asin' => null,
+                    'condition' => 'used',
+                    'brand' => null,
+                    'manufacturer' => null,
+                    'manufacturer_model_number' => null,
+                    'name' => 'Test Product',
+                    'description' => null,
+                    'price' => 0,
+                    'cost' => 0,
+                    'package_length' => 0,
+                    'package_width' => 0,
+                    'package_height' => 0,
+                    'package_unit_of_length' => 'inches',
+                    'package_weight' => 0,
+                    'package_unit_of_weight' => 'pounds',
+                    'msrp' => 0,
+                    'category_name' => '',
+                    'features' => [
+                        'yellow',
+                        'beautiful'
+                    ],
+                    'warranty' => null,
+                    'condition_note' => null,
+                    'upc' => null,
+                    'ean' => null,
+                    'isbn' => null,
+                    'gtin' => null,
+                    'gcid' => null,
+                    'epid' => null,
+                    'image_list' => '',
+                    'custom_attributes' => [
+                        'plate' => 'paper'
+                    ]
+                ]
+            ])
+        );
+    } // End public function testPostProductSuccessfullyPostAProduct
+
+    public function testGetProductTestHigherLimitAmount()
+    {
+        // Get the stored credentials
+        $accountToken = 'am2902ngt3Nn';
+        $secretKey    = 'happy28bananas';
+
+        // Create a mock client object
+        $mockClient = \Mockery::mock(ClientInterface::class);
+
+        // The mock client should receive a request call and it should return at PSR-7 Response object
+        // cotaining JSON
+        $mockClient->shouldReceive('request')->andReturns(
+            new Response(
+                200,
+                [ 'Content-Type' => 'application/json' ],
+                '[{
+                    "id": 1,
+                    "sku": "1903275",
+                    "asin": null,
+                    "condition": "used",
+                    "brand": null,
+                    "manufacturer": null,
+                    "manufacturer_model_number": null,
+                    "name": "Test Product",
+                    "description": null,
+                    "price": 0,
+                    "cost": 0,
+                    "package_length": 0,
+                    "package_width": 0,
+                    "package_height": 0,
+                    "package_unit_of_length": "inches",
+                    "package_weight": 0,
+                    "package_unit_of_weight": "pounds",
+                    "msrp": 0,
+                    "category_name": "",
+                    "features": [
+                        "yellow",
+                        "beautiful"
+                    ],
+                    "warranty": null,
+                    "condition_note": null,
+                    "upc": null,
+                    "ean": null,
+                    "isbn": null,
+                    "gtin": null,
+                    "gcid": null,
+                    "epid": null,
+                    "image_list": "",
+                    "custom_attributes": {
+                        "plate": "paper"
+                    }
+                }]'
+            )
+        );
+
+        $page = 1;
+        $limit = 555;
+        $skuArr = ['1903275'];
+
+        // Create a new instance of DeleteProduct
+        $getProduct = new GetProduct($accountToken, $secretKey, $mockClient);
+
+        // Send the request and get the response object
+        $response = $getProduct->sendRequest($page, $limit, $skuArr);
+
+        $responseJson = (string) $response->getBody();
+
+        // Assert the returned JSON response matches the expected data
+        $this->assertJsonStringEqualsJsonString(
+            $responseJson,
+            json_encode([
+                [
+                    'id' => 1,
+                    'sku' => '1903275',
+                    'asin' => null,
+                    'condition' => 'used',
+                    'brand' => null,
+                    'manufacturer' => null,
+                    'manufacturer_model_number' => null,
+                    'name' => 'Test Product',
+                    'description' => null,
+                    'price' => 0,
+                    'cost' => 0,
+                    'package_length' => 0,
+                    'package_width' => 0,
+                    'package_height' => 0,
+                    'package_unit_of_length' => 'inches',
+                    'package_weight' => 0,
+                    'package_unit_of_weight' => 'pounds',
+                    'msrp' => 0,
+                    'category_name' => '',
+                    'features' => [
+                        'yellow',
+                        'beautiful'
+                    ],
+                    'warranty' => null,
+                    'condition_note' => null,
+                    'upc' => null,
+                    'ean' => null,
+                    'isbn' => null,
+                    'gtin' => null,
+                    'gcid' => null,
+                    'epid' => null,
+                    'image_list' => '',
+                    'custom_attributes' => [
+                        'plate' => 'paper'
+                    ]
+                ]
+            ])
+        );
+    } // End public function testGetProductTestHigherLimitAmount
+
+    public function testGetProductBadCredentialsThrowException()
+    {
+        // Get the stored credentials
+        $accountToken = '';
+        $secretKey    = '';
+
+        // Create a mock client object
+        $mockClient = \Mockery::mock(ClientInterface::class);
+
+        // The mock client should receive a request call and it should return at PSR-7 Response object
+        // cotaining JSON
+        $mockClient->shouldReceive('request')->andReturns(
+            new Response(
+                401,
+                [ 'Content-Type' => 'application/json' ],
+                'HTTP Basic: Access denied.'
+            )
+        );
+
+        $page = 1;
+        $limit = 100;
+        $skuArr = ['1903275'];
+
+        // Create a new instance of DeleteProduct
+        $getProduct = new GetProduct($accountToken, $secretKey, $mockClient);
+
+        // Expect an exception from the request
+        $this->expectException(\Exception::class);
+
+        // Send the request and get the response object
+        $response = $getProduct->sendRequest($page, $limit, $skuArr);
+    } // End public function testGetProductBadCredentialsThrowException
+
+    public function testGetProductThrowDefaultException()
+    {
+        // Get the stored credentials
+        $accountToken = 'M9902nig02';
+        $secretKey    = '22038hgdlahngo234';
+
+        // Create a mock client object
+        $mockClient = \Mockery::mock(ClientInterface::class);
+
+        // The mock client should receive a request call and it should return at PSR-7 Response object
+        // cotaining JSON
+        $mockClient->shouldReceive('request')->andReturns(
+            new Response(
+                405,
+                [ 'Content-Type' => 'application/json' ],
+                'This is the default error.'
+            )
+        );
+
+        $page = 1;
+        $limit = 100;
+        $skuArr = ['1903275'];
+
+        // Create a new instance of DeleteProduct
+        $getProduct = new GetProduct($accountToken, $secretKey, $mockClient);
+
+        // Expect an exception from the request
+        $this->expectException(\Exception::class);
+
+        // Send the request and get the response object
+        $response = $getProduct->sendRequest($page, $limit, $skuArr);
+    } // End public function testGetProductBadCredentialsThrowException
+} // End class GetProductTest
